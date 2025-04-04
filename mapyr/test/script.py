4ce4b59302a991ac9fc5ae50e69548b7b55f76e5
AIG.Livny <AIG.Livny@gmail.com>
2025-04-04 15:45:24 +0300

    fix test

--- mapyr/test/script.py
+++ mapyr/test/script.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 import mapyr
+import os
 
 def run(rule:mapyr.core.Rule) -> int:
-    with open('src/script_artefact.c','w+') as f:
+    with open(f'{os.path.dirname(__file__)}/src/script_artefact.c','w+') as f:
         f.write('int some_fnc(){return 0;}')
     return 0
